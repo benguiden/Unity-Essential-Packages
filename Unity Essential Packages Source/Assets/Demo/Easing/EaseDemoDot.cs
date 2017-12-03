4502b9d26595e6c7dc95261c57c57ea09b6e072f
Ben Guiden <benguiden@hotmail.co.uk>
2017-12-03 23:11:43 +0000

    Added easing demo scene. Added basic easing functions.

---
+++ Unity Essential Packages Source/Assets/Demo/Easing/EaseDemoDot.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class EaseDemoDot : MonoBehaviour {
+
+	public Ease.Functions easingFunction;
+	[Range(-10f, 10f)]
+	public float speed = 0f;
+
+	public float offset;
+
+	private float theta = 0f;
+	private Vector3 originalPosition;
+
+	private void Start(){
+		originalPosition = this.transform.localPosition;
+	}
+
+	private void Update(){
+		float xPos = originalPosition.x + (offset * Ease.EaseValue (theta, easingFunction));
+		this.transform.localPosition = new Vector3 (xPos, originalPosition.y, originalPosition.z);
+
+		theta += speed * Time.deltaTime;
+		if (theta > 0f)
+			theta = theta % 1f;
+		while (theta < 0f) {
+			theta += 1f;
+		}
+
+	}
+
+}
---
+++ Unity Essential Packages Source/Assets/Scripts/Easing/Ease.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class Ease{
+
+	//Main
+	public static float EaseValue(float fraction, Functions function){
+		switch (function) {
+
+		//Linear
+		case Functions.Linear:
+			return fraction;
+
+		//Quad
+		case Functions.QuadIn:
+			return QuadIn (fraction);
+		case Functions.QuadOut:
+			return QuadOut (fraction);
+		case Functions.QuadInOut:
+			return QuadInOut (fraction);
+		
+		//Expo
+
+		}
+
+		return fraction;
+	}
+
+	//Quad
+	public static float QuadIn(float fraction){
+		fraction = Mathf.Clamp01 (fraction);
+		return fraction * fraction;
+	}
+
+	public static float QuadOut(float fraction){
+		fraction = Mathf.Clamp01 (fraction);
+		return -fraction * (fraction - 2f);
+	}
+
+	public static float QuadInOut(float fraction){
+		fraction = Mathf.Clamp01 (fraction);
+		fraction *= 2f;
+		if (fraction < 1f)
+			return 0.5f * fraction * fraction;
+		fraction--;
+		return -0.5f * (fraction * (fraction - 2f) - 1f);
+	}
+
+	[System.Serializable]
+	public enum Functions{
+		Linear,
+		QuadIn, QuadOut, QuadInOut,
+	}
+
+}
