dd3b454b8070db524a453eaecd708a27c17d12ff
Ben Guiden <benguiden@hotmail.co.uk>
2017-12-17 21:26:55 +0000

    Added Controller branch. Added ThirdPersonOrbitController.

---
+++ Unity Essential Packages Source/Assets/Scripts/Controllers/ThirdPersonOrbitController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class ThirdPersonOrbitController : MonoBehaviour {
+
+	#region Public Variables
+	//Camera
+	public Transform lookCamera;
+
+
+	#endregion
+
+	#region Private Variables
+
+	#endregion
+
+	#region Mono Methods
+	private void Awake(){
+		if (lookCamera == null) {
+			if (Camera.main != null) {
+				Debug.LogWarning ("Controller Warning: Look Camera not specified, setting to Main Camera.");
+				lookCamera = Camera.main.transform;
+			} else {
+				Debug.LogError ("Controller Error: Look Camera not specified and no Main Camera in scene.");
+				Debug.Break ();
+			}
+		}
+	}
+	#endregion
+
+}
