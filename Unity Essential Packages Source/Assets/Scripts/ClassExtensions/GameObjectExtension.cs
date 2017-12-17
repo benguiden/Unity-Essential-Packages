2a83880ecb68488c771cd91885ba894d8c768f55
Ben Guiden <benguiden@hotmail.co.uk>
2017-12-17 20:06:42 +0000

    Added class extensions branch.

---
+++ Unity Essential Packages Source/Assets/Scripts/ClassExtensions/GameObjectExtension.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public static class GameObjectExtension{
+
+	public static T GetAddComponent<T>(this GameObject gameObject) where T : Component{
+		if (gameObject == null)
+			return null;
+		T component = gameObject.GetComponent<T> ();
+		if (component == null)
+			component = gameObject.AddComponent<T> ();
+		return component;
+	}
+
+}
